25a5a7c95a12abd7a28369c17fcd97e24b52e522
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-02-18 19:56:37 -0300

    add BaseRepository.php (interface)

---
+++ app/core/Repository/BaseRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Repository;
+
+interface BaseRepository
+{
+  /**
+  * Salva um determinado dado no banco de dados.
+  * @param   array   $data  Array chave-valor, seguindo o padrão :NOME para chave.
+  *
+  * @return  bool
+  */
+  public static function save( array $data ):bool;
+
+  /**
+  * Retorna um array com outro(s) array(s) contendo os dados.
+  *
+  * @return   array
+  */
+  public static function findAll():array;
+
+  /**
+  * Retorna um determinado dado cadastrado em uma tabela do banco de dados.
+  * @param    int   $id   Identificador único para um recurso no banco de dados.
+  * @param    int   $id_s Identifcador secundário (opcional) para tabelas com duas colunas como PK.
+  *
+  * @return   array
+  */
+  public static function findById(int $id, int $id_s = null):array;
+
+  /**
+  * Atualiza um determinado recurso no banco de dados.
+  * @param   array   $data  Array chave-valor, com o ID.
+  *
+  * @return  bool
+  */
+  public static function update(array $data):bool;
+
+  /**
+  * Remove um determinado recurso do banco de dados.
+  * @param   int   $id  Identificador do recurso.
+  *
+  * @return  bool
+  */
+  public static function delete(int $id):bool;
+}
+
+ ?>
