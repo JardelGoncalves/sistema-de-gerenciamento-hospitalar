b62db8ee63807afb3d4bef9351fff98cc23f277d
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-02-18 01:12:21 -0300

    add db.sql (esquema de tabelas)

---
+++ pg/initial_pg/db.sql
@@ -0,0 +1,42 @@
+\c hms;
+
+CREATE TABLE usuarios (
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(200),
+  email VARCHAR(255),
+  senha VARCHAR(100)
+);
+
+CREATE TABLE planos (
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(100),
+  valor REAL
+);
+
+CREATE TABLE pacientes (
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(255),
+  cpf VARCHAR(11),
+  rua VARCHAR(255),
+  bairro VARCHAR(100),
+  cidade VARCHAR(100),
+  estado VARCHAR(100),
+  cep VARCHAR(20),
+  telefone VARCHAR(20),
+  plano_id INTEGER,
+  FOREIGN KEY (plano_id) REFERENCES planos (id)
+);
+
+
+CREATE TABLE medicos (
+  id SERIAL PRIMARY KEY,
+  nome VARCHAR(255),
+  crm INTEGER
+);
+
+CREATE TABLE consultas(
+  paciente_id INTEGER NOT NULL,
+  medico_id INTEGER NOT NULL,
+  data_marcada TIMESTAMP,
+  PRIMARY KEY (paciente_id, medico_id)
+)
