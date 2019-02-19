0da691af496c7b031c0fdbfe63439f03fcbdfe5f
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-02-19 00:17:32 -0300

    add NOT NULL nos campos obrigatórios

--- pg/initial_pg/db.sql
+++ pg/initial_pg/db.sql
@@ -2,27 +2,27 @@
 
 CREATE TABLE usuarios (
   id SERIAL PRIMARY KEY,
-  nome VARCHAR(200),
-  email VARCHAR(255),
-  senha VARCHAR(100)
+  nome VARCHAR(200) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  senha VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE planos (
   id SERIAL PRIMARY KEY,
-  nome VARCHAR(100),
-  valor REAL
+  nome VARCHAR(100) NOT NULL,
+  valor REAL NOT NULL
 );
 
 CREATE TABLE pacientes (
   id SERIAL PRIMARY KEY,
-  nome VARCHAR(255),
-  cpf VARCHAR(11),
-  rua VARCHAR(255),
-  bairro VARCHAR(100),
-  cidade VARCHAR(100),
-  estado VARCHAR(100),
-  cep VARCHAR(20),
-  telefone VARCHAR(20),
+  nome VARCHAR(255) NOT NULL,
+  cpf VARCHAR(11) NOT NULL,
+  rua VARCHAR(255) NOT NULL,
+  bairro VARCHAR(100) NOT NULL,
+  cidade VARCHAR(100) NOT NULL,
+  estado VARCHAR(100) NOT NULL,
+  cep VARCHAR(20) NOT NULL,
+  telefone VARCHAR(20) NOT NULL,
   plano_id INTEGER NOT NULL,
   FOREIGN KEY (plano_id) REFERENCES planos (id)
 );
@@ -30,14 +30,14 @@ CREATE TABLE pacientes (
 
 CREATE TABLE medicos (
   id SERIAL PRIMARY KEY,
-  nome VARCHAR(255),
-  especialidades VARCHAR(255),
-  crm INTEGER
+  nome VARCHAR(255) NOT NULL,
+  especialidades VARCHAR(255) NOT NULL,
+  crm INTEGER NOT NULL
 );
 
 CREATE TABLE consultas (
   paciente_id INTEGER NOT NULL,
   medico_id INTEGER NOT NULL,
-  data_marcada TIMESTAMP,
+  data_marcada TIMESTAMP NOT NULL,
   PRIMARY KEY (paciente_id, medico_id)
 )
