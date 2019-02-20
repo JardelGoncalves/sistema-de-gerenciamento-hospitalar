590e8f073fb07895542de0e4c57eb1fc2cb6c14e
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-02-20 02:23:59 -0300

    movendo contúdo para Views/login.php e preparando index.php para include das páginas

--- app/index.php
+++ app/index.php
@@ -1,53 +1,18 @@
 <?php
-require 'config.php';
-unset($_SESSION['usuario']);
-require 'include/auth.php';
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <title>Sistema de Gerenciamento Hospitalar</title>
-    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-  </head>
-  <body style="background:#f3f3f3">
 
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-12">
-          <div style="margin-top:8%">
-            <div class="text-center" style="margin-bottom:20px">
-              <img class="img-fluid" width="300" src="/assets/img/logo.png">
-            </div>
+require 'include/friendly_url.php';
 
-            <?
-            if($_SESSION['erro']) {
-            ?>
-            <div class="alert alert-danger" role="alert">
-              <?echo $_SESSION['erro'];?>
-            </div>
-            <?
-            };
-            unset($_SESSION['erro']);
-            ?>
-            <form method="post">
-              <div class="form-group">
-                <label for="exampleInputEmail1">Email</label>
-                <input type="email" name="email" class="form-control" placeholder="Email">
-              </div>
-              <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" name="senha" class="form-control" placeholder="Password">
-              </div>
-              <div class="text-center">
-                <button type="submit" class="btn-block btn btn-primary">Acessar</button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="/assets/js/bootstrap.min.js"></script>
-  </body>
-</html>
+$REQUEST_URI = filter_input(INPUT_SERVER, 'REQUEST_URI');
+$POS = strpos($REQUEST_URI, '?'); //pos do ? na url (caso exista)
+
+if ($POS): // se existir
+  $REQUEST_URI = substr($REQUEST_URI, 0, $POS); // retorna apenas a url (sem query string)
+endif;
+
+$REQUEST_URI = substr($REQUEST_URI, 1); // remove a / (raiz) incial da url
+
+$URL = explode('/', $REQUEST_URI); // cria um array com todos os niveis da url
+
+$URL[0] = ($URL[0] !== '' ? $URL[0] : 'login');
+
+include file_include($URL);
