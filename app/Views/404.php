a79dc4d286ea8d72ec908a79416bff6a830b3f03
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-02-20 13:38:02 -0300

    adicionando página 404.php

---
+++ app/Views/404.php
@@ -0,0 +1,9 @@
+<?php
+require 'include/page/head.php';
+?>
+<div class="text-center" style="margin-top:10%">
+  <img class="img-fluid" src="/assets/img/404.png" width="500px" alt="">
+</div>
+<?php
+require 'include/page/footer.php';
+?>
