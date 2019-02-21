6c282108bf78fe9414a8ea6b58697bc41ddea85a
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-02-20 22:10:04 -0300

    listar planos

---
+++ app/Views/dashboard/plano/listar.php
@@ -0,0 +1,33 @@
+<?php
+require 'config.php';
+require 'include/page/security.php';
+
+use Repository\PlanoRepository;
+$planos = PlanoRepository::findAll();
+
+require 'include/page/head.php';
+?>
+
+<div class="col-md-8 offset-md-2 col-sm-12" style="margin-top:80px">
+  <?require 'include/page/sucesso.php';?>
+  <div class="row">
+    <?
+    foreach ($planos as $plano) {
+    ?>
+    <div class="card" style="width: 14rem;">
+      <div class="text-center">
+        <img width="80%" src="/assets/img/plan.png" style="margin-top:10px" alt="Card image cap">
+      </div>
+      <div class="card-body text-center">
+        <h5 class="card-title"><? echo $plano['nome']; ?></h5>
+        <p><strong>Valor: </strong> R$ <? echo $plano['valor']; ?></p>
+        <a href="/dashboard/plano/remover?id=<? echo $plano['id']; ?>" class="btn btn-danger"><i class="fas fa-trash"></i></a>
+      </div>
+    </div>
+    <? } ?>
+  </div>
+</div>
+
+<?php
+require 'include/page/footer.php';
+?>
